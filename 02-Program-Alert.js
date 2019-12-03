b9cb4510c3c86e62cbe1c719abb78b2851d5ab24
jross111 <jross.nyc111@gmail.com>
2019-12-03 14:04:58 -0500

    :boom:

--- 02-Program-Alert.js
+++ 02-Program-Alert.js
@@ -1,81 +1,144 @@
 const readline = require("readline");
 const fs = require("fs");
 
+// const HALT = 99;
+// const ADD = 1;
+// const MULTIPLY = 2;
+
+// function initProgram() {
+//   let program = fs.readFileSync("./input-02.txt", "utf-8").split(",");
+//   //let program = fs.readFileSync("./day-02/test.txt", 'utf-8').split(',');
+//   // program[1] = noun;
+//   // program[2] = verb;
+//   console.log(program);
+//   return program;
+// }
+
+// console.log(initProgram().length);
+
+// //after process is complete
+// function incrementIndex(index) {
+//   return (index += 4);
+// }
+
+// // start, and after increment
+// function getOpcode(program, index) {
+//   return parseInt(program[index]);
+// }
+
+// function getFirstValue(program, index) {
+//   return parseInt(program[program[index]]);
+// }
+
+// function getSecondValue(program, index) {
+//     return parseInt(program[program[index + 1]])
+// }
+
+// function getPosition(program, index) {
+//     return parseInt(program[program[index + 2]])
+// }
+
+// function part1() {
+//     program = initProgram();
+
+//     let index = 0;
+//     let opcode = getOpcode(program, index);
+//     let firstValue = getFirstValue(program, index);
+//     let secondValue = getSecondValue(program, index);
+//     let newPosition = getPosition(program, index);
+
+//     while(opcode !== HALT) {
+
+//         if(opcode === ADD) {
+//             program[newPosition] = firstValue + secondValue;
+//         } else if( opcode === MULTIPLY) {
+//             program[newPosition] = firstValue * secondValue;
+//         } else {
+//             console.error("oopsy")
+//             break;
+//         }
+
+//         index = incrementIndex(index);
+//         opcode = getOpcode(program, index);
+//         firstValue = getFirstValue(program, index);
+//         secondValue = getSecondValue(program, index);
+//         newPosition = getPosition(program, index);
+
+   
+//     }
+ 
+//     return program[0];
+
+
+// }
+
+// console.log(part1())
+
 const HALT = 99;
 const ADD = 1;
 const MULTIPLY = 2;
 
 function initProgram() {
-  let program = fs.readFileSync("./input-02.txt", "utf-8").split(",");
+  let program = fs.readFileSync("./input-02.txt", 'utf-8').split(',');
   //let program = fs.readFileSync("./day-02/test.txt", 'utf-8').split(',');
-  // program[1] = noun;
-  // program[2] = verb;
-  console.log(program);
+//   program[1] = noun;
+//   program[2] = verb;
   return program;
 }
 
-console.log(initProgram().length);
-
-//after process is complete
 function incrementIndex(index) {
-  return (index += 4);
+  return index += 4;
 }
 
-// start, and after increment
 function getOpcode(program, index) {
   return parseInt(program[index]);
 }
 
-function getFirstValue(program, index) {
-  return parseInt(program[program[index]]);
+function getValueOne(program, index) {
+  return parseInt(program[program[index + 1]]);
 }
 
-function getSecondValue(program, index) {
-    return parseInt(program[program[index + 1]])
+function getValueTwo(program, index) {
+  return parseInt(program[program[index + 2]]);
 }
 
 function getPosition(program, index) {
-    return parseInt(program[program[index + 2]])
+  return parseInt(program[index + 3]);
 }
 
 function part1() {
-    program = initProgram();
+  
+  program = initProgram();
 
-    let index = 0;
-    let opcode = getOpcode(program, index);
-    let firstValue = getFirstValue(program, index);
-    let secondValue = getSecondValue(program, index);
-    let newPosition = getPosition(program, index);
+  let index = 0;
+  let opcode = getOpcode(program, index);
+  let valueOne = getValueOne(program, index);
+  let valueTwo = getValueTwo(program, index);
+  let position = getPosition(program, index);
 
-    while(opcode !== HALT) {
+  while(opcode !== HALT) {
 
-        if(opcode === ADD) {
-            program[newPosition] = firstValue + secondValue;
-        } else if( opcode === MULTIPLY) {
-            program[newPosition] = firstValue * secondValue;
-        } else {
-            console.error("oopsy")
-            break;
-        }
-
-        index = incrementIndex(index);
-        opcode = getOpcode(program, index);
-        firstValue = getFirstValue(program, index);
-        secondValue = getSecondValue(program, index);
-        newPosition = getPosition(program, index);
-
-   
+    if(opcode === ADD) {
+      program[position] = valueOne + valueTwo;
+    } else if( opcode === MULTIPLY) {
+      program[position] = valueOne * valueTwo;
+    } else {
+      console.error("opcode error")
+      break;
     }
- 
-    return program[0];
 
+    index = incrementIndex(index);
+    opcode = getOpcode(program, index);
+    valueOne = getValueOne(program, index);
+    valueTwo = getValueTwo(program, index);
+    position = getPosition(program, index);
+  }
 
+  return program[0];
 }
 
+
 console.log(part1())
-
-
-
 
 
 
